c40fdcec9ef5a211a0a4a52e1523d9a27643d11c
Huw Lewis <huwtlewis@gmail.com>
2012-07-20 16:32:57 +0100

    Use inject instead of each_with_object for better compaitbility with older ruby versions.

--- lib/secure_yaml/yaml_decrypter.rb
+++ lib/secure_yaml/yaml_decrypter.rb
@@ -13,7 +13,7 @@ module SecureYaml
     def decrypt(yaml)
       case yaml
         when Hash
-          yaml.each_with_object({}) {|(key, value), new_hash| new_hash[key] = decrypt(value)}
+          yaml.inject({}) {|new_hash, (key, value)| new_hash[key] = decrypt(value); new_hash}
         when String
           yaml.gsub(/^#{ENCRYPTED_PROPERTY_WRAPPER_ID}\((.*)\)$/) {@cipher.decrypt(@secret_key, $1)}
         else
