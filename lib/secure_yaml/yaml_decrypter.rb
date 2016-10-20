6fe22c68fb22a2791fb0168d6eb9a938b452901a
Andre Duffeck <aduffeck@suse.com>
2016-10-20 09:06:33 +0200

    Add support for decrypting values in array elements

--- lib/secure_yaml/yaml_decrypter.rb
+++ lib/secure_yaml/yaml_decrypter.rb
@@ -15,6 +15,8 @@ module SecureYaml
           yaml.inject({}) {|new_hash, (key, value)| new_hash[key] = decrypt(value); new_hash}
         when String
           yaml.gsub(/\b#{ENCRYPTED_PROPERTY_WRAPPER_ID}\((.*)\)(?:\b|$)/) {@decryption_algorithm.decrypt(@secret_key, $1)}
+        when Array
+          yaml.map {|element| decrypt(element)}
         else
           yaml
       end
